c4f88f67d430a0e8b8f71e3bd5556a07bbf3227d
media-frameworks <mikehallvideo@gmail.com>
2025-11-02 19:48:57 -0500

    coverage from server

--- tile/FractoTileCoverage.jsx
+++ tile/FractoTileCoverage.jsx
@@ -3,15 +3,11 @@ import PropTypes from 'prop-types';
 import styled from "styled-components";
 
 import {CoolStyles} from "common/ui/CoolImports";
-import FractoIndexedTiles from "fracto/common/data/FractoIndexedTiles";
 import CoolTable, {
-   CELL_ALIGN_CENTER, CELL_TYPE_LINK,
-   CELL_TYPE_NUMBER,
-   TABLE_CAN_SELECT
+   CELL_ALIGN_CENTER,
+   CELL_TYPE_NUMBER
 } from "common/ui/CoolTable";
-import {get_ideal_level} from "../data/FractoData";
-import {INSPECTOR_SIZE_PX} from "pages/constants";
-import FractoUtil from "../FractoUtil";
+import network from "common/config/network.json"
 
 const SectionWrapper = styled(CoolStyles.Block)`
     ${CoolStyles.align_center}
@@ -23,6 +19,8 @@ const SectionWrapper = styled(CoolStyles.Block)`
 const LinkedCell = styled(CoolStyles.InlineBlock)`
     margin: 0;
 `
+
+const IMAGE_SERVER_URL = network.image_server_url
 
 const COVERAGE_TABLE_COLUMNS = [
    {
@@ -76,9 +74,6 @@ const COVERAGE_TABLE_COLUMNS = [
    },
 ]
 
-const MAX_LEVELS = 30; // Limit the number of levels processed
-const MAX_TILES_PER_LEVEL = 150000; // Limit tiles per level to avoid memory issues
-
 export class FractoTileCoverage extends Component {
 
    static propTypes = {
@@ -91,106 +86,37 @@ export class FractoTileCoverage extends Component {
    }
 
    state = {
-      tiles_in_scope: [],
-      tile_index: 0,
-      all_history: [],
       enhance_tiles: [],
       repair_tiles: [],
-      enhance_level: 0,
-      repair_level: 0,
-      loading_indexed: true,
-      loading_blanks: true,
-      loading_interiors: true,
-      loading_needs_update: true,
-      repairs_by_level: {},
-      coverage: [],
+      coverage: null,
       render_details: false,
    }
 
-   static indexed_tiles = []
-   static blank_tiles = []
-   static interior_tiles = []
-   static needs_update = []
-
    componentDidMount() {
-      this.collect_category_tiles('indexed', result => {
-         FractoTileCoverage.indexed_tiles = result;
-         this.setState({loading_indexed: false})
-      })
-      this.collect_category_tiles('blank', result => {
-         FractoTileCoverage.blank_tiles = result;
-         this.setState({loading_blanks: false})
-      })
-      this.collect_category_tiles('interior', result => {
-         FractoTileCoverage.interior_tiles = result;
-         this.setState({loading_interiors: false})
-      })
-      this.collect_category_tiles('needs_update', result => {
-         FractoTileCoverage.needs_update = result;
-         this.setState({loading_needs_update: false})
-      })
-   }
-
-   collect_category_tiles = (tile_set_name, cb) => {
-      const result = []
-      for (let level = 0; level < 30; level++) {
-         result[level] = new Set()
-      }
-      FractoIndexedTiles.load_short_codes(tile_set_name, short_codes => {
-         short_codes.forEach(short_code => {
-            const level = short_code.length
-            if (!result[level]) {
-               return;
-            }
-            result[level].add(short_code)
-         })
-         cb(result)
-      })
+      this.fetch_coverage()
    }
 
    componentDidUpdate(prevProps: Readonly<P>, prevState: Readonly<S>, snapshot: SS) {
-      const {loading_blanks, loading_indexed} = this.state
       const {focal_point, scope} = this.props
       const focal_point_x_changed = focal_point.x !== prevProps.focal_point.x
       const focal_point_y_changed = focal_point.y !== prevProps.focal_point.y
       const scope_changed = scope !== prevProps.scope
-      const loading_indexed_changed = loading_indexed !== prevState.loading_indexed
-      const loading_blanks_changed = loading_blanks !== prevState.loading_blanks
       if (focal_point_y_changed
          || focal_point_x_changed
-         || scope_changed
-         || (!loading_blanks && !loading_indexed
-            && (loading_blanks_changed || loading_indexed_changed)
-         )
-      ) {
-         this.detect_coverage()
+         || scope_changed) {
+         // this.detect_coverage()
+         // const coverage = detect_coverage(focal_point, scope)
+         // this.setState({coverage})
+         this.fetch_coverage()
       }
    }
 
-   detect_coverage = () => {
-      const {focal_point, scope} = this.props;
-      const tiles_in_scope = [];
-      const ideal_level = get_ideal_level(INSPECTOR_SIZE_PX, scope, 1.5);
-      const max_levels = Math.min(MAX_LEVELS, ideal_level + 10); // Only process a few levels
-
-      for (let level = 2; level < max_levels; level++) {
-         const level_tiles = FractoIndexedTiles.tiles_in_scope(level, focal_point, scope);
-         if (level_tiles.length > MAX_TILES_PER_LEVEL) {
-            // Limit the number of tiles per level
-            level_tiles.length = MAX_TILES_PER_LEVEL;
-         }
-         console.log('level_tiles', level, level_tiles)
-         tiles_in_scope.push({
-            level: level,
-            tiles: level_tiles
-         });
-      }
-      const filtered_tiles_in_scope = tiles_in_scope.filter(scoped => scoped.tiles.length > 1);
-      const coverage = this.render_coverage(filtered_tiles_in_scope);
-      this.setState({
-         coverage,
-         tiles_in_scope: filtered_tiles_in_scope
-      });
+   fetch_coverage = async () => {
+      const {focal_point, scope} = this.props
+      const url = `${IMAGE_SERVER_URL}/tile_coverage?focal_point=${JSON.stringify(focal_point)}&scope=${scope}`
+      const result = await fetch(url).then(res => res.json())
+      console.log('fetch_coverage', result.coverage)
+      this.setState({coverage: result.coverage})
    }
 
    set_enhanced = (enhance_tiles, level, add_interiors) => {
@@ -205,12 +131,6 @@ export class FractoTileCoverage extends Component {
       on_tile_set_changed(repair_tiles, level, true, false)
    }
 
-   set_can_detail = (detail_tiles, level) => {
-      const {on_tile_set_changed} = this.props
-      this.setState({render_details: true})
-      on_tile_set_changed(detail_tiles, level, false, true)
-   }
-
    on_select_row = (row) => {
       console.log('row', row)
       const {selected_level, on_level_selected} = this.props
@@ -221,116 +141,52 @@ export class FractoTileCoverage extends Component {
       }
    }
 
-   render_coverage = (tiles_in_scope) => {
-      const {selected_level} = this.props;
-      let all_tiles = [];
-      const coverage_data = tiles_in_scope.map(scoped => {
-         all_tiles = all_tiles.concat(scoped.tiles);
-         return {
-            level: scoped.level,
-            tile_count: scoped.tiles.length,
-            tiles: scoped.tiles,
-         };
-      });
-      if (tiles_in_scope.length) {
-         coverage_data.push({
-            level: tiles_in_scope[tiles_in_scope.length - 1].level + 1,
-            tile_count: 0,
-            tiles: [],
-         });
+   render_coverage = () => {
+      const {coverage} = this.state
+      if (!Array.isArray(coverage)) {
+         console.log('coverage is not an array', coverage)
+         return []
       }
-      // Use Sets for faster lookups and to avoid duplicates
-      const can_do = new Set();
-      const blank_tiles = new Set();
-      const interior_tiles = new Set();
-      const needs_update = new Set();
-      all_tiles.forEach(tile => {
-         const short_code = tile.short_code;
-         const level = short_code.length
-         const in_update = FractoTileCoverage.needs_update[level].has(short_code);
-         if (in_update) {
-            needs_update.add(short_code);
-         }
-         const new_level = short_code.length + 1;
-         for (let i = 0; i < 4; i++) {
-            const sc = `${short_code}${i}`;
-            const in_blank = FractoTileCoverage.blank_tiles[new_level].has(sc);
-            if (in_blank) {
-               blank_tiles.add(sc);
-            } else {
-               const in_indexed = FractoTileCoverage.indexed_tiles[new_level].has(sc);
-               const in_interior = FractoTileCoverage.interior_tiles[new_level].has(sc);
-               if (in_interior && !in_indexed) {
-                  interior_tiles.add(sc);
-               } else {
-                  if (!in_indexed) {
-                     can_do.add(sc);
-                  }
-               }
-            }
-         }
-      });
-      coverage_data.forEach(data => {
-         const filtered_by_level = Array.from(can_do).filter(cd => cd.length === data.level).map(short_code => ({
-            short_code,
-            bounds: FractoUtil.bounds_from_short_code(short_code)
-         }));
-         const blanks_by_level = Array.from(blank_tiles).filter(short_code => short_code.length === data.level);
-         const interiors_by_level = Array.from(interior_tiles).filter(short_code => short_code.length === data.level);
-         const needs_update_by_level = Array.from(needs_update).filter(short_code => short_code.length === data.level);
-         const interiors_with_bounds = interiors_by_level.map(short_code => ({
-            short_code,
-            bounds: FractoUtil.bounds_from_short_code(short_code)
-         }));
-         const needs_update_with_bounds = needs_update_by_level.map(short_code => ({
-            short_code,
-            bounds: FractoUtil.bounds_from_short_code(short_code)
-         }));
-         data.can_do = filtered_by_level.length ? <LinkedCell
-            onClick={e => this.set_enhanced(filtered_by_level, data.level, false)}>
-            <CoolStyles.LinkSpan>{filtered_by_level.length}</CoolStyles.LinkSpan>
+      const coverage_data = coverage.map(data => {
+         data.can_do = data.filtered_by_level.length ? <LinkedCell
+            onClick={e => this.set_enhanced(data.filtered_by_level, data.level, false)}>
+            <CoolStyles.LinkSpan>{data.filtered_by_level.length}</CoolStyles.LinkSpan>
          </LinkedCell> : '-';
-         data.blank_tiles = blanks_by_level.length ? blanks_by_level.length : '-';
-         data.interior_tiles = interiors_with_bounds.length
+         data.blank_tiles = data.blanks_by_level.length ? data.blanks_by_level.length : '-';
+         data.interior_tiles = data.interiors_with_bounds.length
             ? <LinkedCell
-               onClick={e => this.set_enhanced(interiors_with_bounds, data.level, true)}>
-               <CoolStyles.LinkSpan>{interiors_with_bounds.length}</CoolStyles.LinkSpan>
+               onClick={e => this.set_enhanced(data.interiors_with_bounds, data.level, true)}>
+               <CoolStyles.LinkSpan>{data.interiors_with_bounds.length}</CoolStyles.LinkSpan>
             </LinkedCell>
             : '-';
-         data.needs_update_tiles = needs_update_with_bounds.length
+         data.needs_update_tiles = data.needs_update_with_bounds.length
             ? <LinkedCell
-               onClick={e => this.set_can_repair(needs_update_with_bounds, data.level)}>
-               <CoolStyles.LinkSpan>{needs_update_with_bounds.length}</CoolStyles.LinkSpan>
+               onClick={e => this.set_can_repair(data.needs_update_with_bounds, data.level)}>
+               <CoolStyles.LinkSpan>{data.needs_update_with_bounds.length}</CoolStyles.LinkSpan>
             </LinkedCell>
             : '-';
          data.tile_count = data.tiles.length ? <LinkedCell
             onClick={e => this.set_can_repair(data.tiles, data.level)}>
             <CoolStyles.LinkSpan>{data.tiles.length}</CoolStyles.LinkSpan>
          </LinkedCell> : '-';
+         return data
       });
-      const selected_row = selected_level
-         ? coverage_data.findIndex(data => data.level === selected_level)
-         : -1;
+
       return <CoolStyles.InlineBlock>
          <CoolTable
-            options={TABLE_CAN_SELECT}
             data={coverage_data}
             columns={COVERAGE_TABLE_COLUMNS}
-            on_select_row={row => this.on_select_row(coverage_data[row])}
-            selected_row={selected_row}
          />
       </CoolStyles.InlineBlock>;
    }
 
    render() {
-      const {
-         loading_blanks, loading_indexed, loading_interiors, loading_needs_update,
-         coverage
-      } = this.state
-      if (loading_blanks || loading_indexed || loading_interiors || loading_needs_update) {
+      const {coverage} = this.state
+      if (!coverage) {
          return "loading short codes..."
       }
-      return <SectionWrapper>{coverage}</SectionWrapper>
+      const coverage_data = this.render_coverage()
+      return <SectionWrapper>{coverage_data}</SectionWrapper>
    }
 }
 
